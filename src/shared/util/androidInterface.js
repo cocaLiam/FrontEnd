d44dedd2f32bf7d730520d9ae2fcca7dcffda89b
liam <liam@cocabot.com>
2024-12-09 22:50:19 +0900

    Release_v1.2 [프로토콜 테스트중]

--- src/shared/util/androidInterface.js
+++ src/shared/util/androidInterface.js
@@ -1,7 +1,7 @@
 const andInterface = {
-/**
- * ######################## Web ---API---> Android Function 호출 ########################
- */
+  /**
+   * ######################## Web ---API---> Android Function 호출 ########################
+   */
   // Android WebView의 showToast 호출
   showToast: (msg) => {
     if (window.AndroidInterface && window.AndroidInterface.andShowToast) {
@@ -12,32 +12,35 @@ const andInterface = {
   },
 
 
-/**
- * ################## Android ---JsonObject---> Web 으로 DataInfo 전달  ##################
- */
-  // Android WebView에서 호출할 수 있도록 window 객체에 함수 등록
-  // registerAndroidInterface: (callback) => {
-  //   window.receiveDataFromApp = (data) => {
-  //     try {
-  //       const parsedData = JSON.parse(data); // JSON 데이터 파싱
-  //       callback(parsedData);
-  //     } catch (error) {
-  //       console.error(`Json String Type 이 아닙니다. : ${data}`);
-  //     }
-  //   };
-  // }
-  
+  /**
+   * ################## Android ---JsonObject---> Web 으로 DataInfo 전달  ##################
+   */
   // 실제 receiveDataFromApp 함수 정의
   receiveDataFromApp: (data) => {
     try {
       const parsedData = JSON.parse(data); // JSON 데이터 파싱
-      console.log("Parsed Data from Android: ", parsedData);
-      // 필요한 추가 로직 작성
+      console.log("Parsed Data from Android: ", JSON.stringify(parsedData, null, 2));
+      return true  // Android 쪽에 return 값 반환
     } catch (error) {
       console.error(`Json String Type 이 아닙니다. : ${data}`);
+      return false // Android 쪽에 return 값 반환
+    }
+  },
+
+  resConnect: (data) => {
+    // resConnect(macAddress: String, deviceName: String)
+    try {
+      console.log(JSON.stringify(data, null, 2))
+      console.log(data)
+      console.log(data.macAddress)
+      console.log(data.deviceName)
+      return true  // Android 쪽에 return 값 반환
+    } catch (error) {
+      console.error(`Json String Type 이 아닙니다. : ${data}`);
+      return false // Android 쪽에 return 값 반환
     }
   }
 }
-  
+
 
 export default andInterface;
