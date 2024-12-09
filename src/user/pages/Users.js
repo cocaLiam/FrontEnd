dcd76076aa31fe0a7e61266ce157bf103c8e44b9
liam <liam@cocabot.com>
2024-12-09 20:16:22 +0900

    Release_v1.2 [BackEnd 주소 변경]

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -28,8 +28,17 @@ const Users = () => {
     // // Android WebView에서 호출할 수 있도록 window 객체에 함수 등록
     // andInterface.registerAndroidInterface(subAndroidData);
     // window 객체에 특정 함수만 등록
-    window = window || {}; // andInterface 네임스페이스가 없으면 생성
-    window.receiveDataFromApp = andInterface.receiveDataFromApp; // 특정 함수만 등록
+
+    // //  window.receiveDataFromApp << Android 에 정의된 함수명
+    // window.andInterface = window.andInterface || {}; // andInterface 네임스페이스가 없으면 생성
+    // window.andInterface.receiveDataFromApp = andInterface.receiveDataFromApp; // 특정 함수만 등록
+
+    // // receiveDataFromApp 함수를 전역에 등록
+    // window.andInterface = window.andInterface || {}; // andInterface 네임스페이스가 없으면 생성
+    // window.andInterface.receiveDataFromApp = andInterface.receiveDataFromApp; // 특정 함수만 등록
+
+    window.receiveDataFromApp = andInterface.receiveDataFromApp;
+
   }, [sendRequest]);
 
   return (
